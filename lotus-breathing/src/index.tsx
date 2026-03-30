c2b0467ba474a4ecd1006bd59906bd3cece89b16
Steveny38 <sy.steveny@gmail.com>
2026-03-29 21:31:02 -0400

    lotus-breathing

---
+++ lotus-breathing/src/index.tsx
@@ -0,0 +1,30 @@
+/**
+ * @file   index.tsx
+ * @brief  Intial component for the react app
+ * @date   Feb , 2020
+ * @author ZCO Engineer
+ * @copyright (c) 2020, ZCO
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
+import Breathe from "./components/Breathe";
+import "./index.css";
+import "material-icons";
+const eventMethod = window.addEventListener
+  ? "addEventListener"
+  : "attachEvent";
+const eventer = window[eventMethod];
+const messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
+eventer(
+  messageEvent,
+  (e) => {
+    ReactDOM.render(
+      <AppContainer>
+        <Breathe data={e.data} />
+      </AppContainer>,
+      document.getElementById("root") as HTMLElement
+    );
+  },
+  false
+);
